378b700c0fcedcbdcadfd446ec6c75f2afdcfa1d
Arthur Tumanyan <arthurtumanyan@yahoo.com>
2014-07-02 17:03:44 +0400

    Changed README and makeinstall.sh

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-webrados
-========
+Webrados
+---------
+This software provides REST access to CEPH storage directly. 
+It does not provide any extra possibilities, like RadosGateway,
+just add(append mode), fetch, delete and stat.
 
-REST access to Rados 
+USAGE
+	Upload: curl -XPUT -T filename http://uploadhost.com/poolname/filename
+	Download: curl -XGET http://uploadhost.com/poolname/filename
+	Delete: curl -XDELETE http://uploadhost.com/poolname/filename
+	Stat: curl -XOPTIONS http://uploadhost.com/poolname/filename
+
+DEPENDENCIES
+	libpthread
+	libfcgi
+	librados
+	libconfig
--- makeinstall.sh
+++ makeinstall.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 :
 rm -rf *.o
-test -f /var/lib/apache2/fastcgi/radosweb.fcgi && rm /var/lib/apache2/fastcgi/radosweb.fcgi
+test -f bin/radosweb.fcgi && rm bin/radosweb.fcgi
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o config.o config.c
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o logs.o logs.c
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o memory.o memory.c
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o method.o method.c
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o signal.o signal.c
 gcc -Wall  -zmuldefs -g -ggdb -O0    -c -g -I/usr/include/rados -lconfig -o utceph.o utceph.c
-mkdir -p /var/lib/apache2/fastcgi
-gcc -Wall  -zmuldefs -g -ggdb -O0     -o /var/lib/apache2/fastcgi/radosweb.fcgi config.o logs.o memory.o method.o signal.o utceph.o /usr/lib/libfcgi.so /usr/lib/librados.so -lconfig -lpthread   
+mkdir -p bin
+gcc -Wall  -zmuldefs -g -ggdb -O0     -o bin/radosweb.fcgi config.o logs.o memory.o method.o signal.o utceph.o /usr/lib/libfcgi.so /usr/lib/librados.so -lconfig -lpthread   
  
